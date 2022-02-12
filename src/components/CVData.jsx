62029b14a2becf60598fcbb9eca3b9a355ed8394
Farragut West <fargutvest@gmail.com>
2022-02-12 17:40:09 +0300

    full filled CV data

--- src/components/CVData.jsx
+++ src/components/CVData.jsx
@@ -101,7 +101,7 @@ export const CVData = {
       type: 'tag-list',
       title: 'Skills',
       icon: 'rocket',
-      items: ['.NET', 'C#', 'WPF', 'Java', 'Spring', 'React', 'SQL']
+      items: ['.NET', 'C#', 'WPF', 'WinForms', 'Xamarin', 'Silverlight', 'Delphi', 'X-Ray', 'Andriod', 'Java', 'Spring', 'React', 'Angular 4', 'Typescript', 'SQL', 'MySQL', 'Kafka', 'ELK']
     },
     {
       type: 'tag-list',
