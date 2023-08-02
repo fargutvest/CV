407f71ecbe7b8fe61324d72986f2fc5734ce4537
Farragut West <fargutvest@gmail.com>
2023-08-02 17:15:25 +0300

    updated cv

--- src/components/CVData.jsx
+++ src/components/CVData.jsx
@@ -31,97 +31,108 @@ export const CVData = {
       icon: 'archive',
       items: [
 	   {
-          title: 'Software Designer',
+          title: 'Senior Software Engineer',
           company: 'Globant',
-          companyMeta: 'This is out-source copmpany headquarted in Argentina and specializing on out-sourcing.',
+          companyMeta: 'Out-source company headquarted in Argentina',
           description:
-          'Worked on 1 project with UK customer. ' +
-          'This is Procedural World toolset for AAA Game dev team. ' + 
-          'Used .NET C# and WPF as main technologies. ' +
+          'Worked on 1 project with the customer from the UK. ' +
+          'It was Procedural World toolset for AAA Game dev team. ' + 
+          'Used .NET C#, WPF, SharpMake, Helix Perforce/P4 as main technologies. ' +
           'Leaded development process, promoted architectural improvements. ' +
-          'Participated daily SCRUM video-calls with customer. ' + 
+          'Implemented features, fixed bugs, investigated issues, wrote UnitTests, reviewed code. ' +
+          'Participated in daily SCRUM video-calls with the customer in English. ' + 
           'Reported about my work and discussed techical topics in English. ' +
           'Mentored Junior teammates. ',
           companyWebSite: 'https://www.globant.com/',
           datesBetween: '2022.04 - Present time',
-          descriptionTags: ['.NET', 'C#', 'WPF', 'C++', 'Houdini']
+          descriptionTags: ['.NET', 'C#', 'WPF', 'C++', 'Houdini', "SharpMake", "Asure DevOps", "TeamCity", "Helix Perforce/P4", "Swarm"]
         },
         {
           title: 'Senior Software Engineer',
           company: 'ICE Mortgage Technology',
-          companyMeta: 'This is product copmpany headquarted in USA and specializing on mortagage.',
+          companyMeta: 'Product company headquarted in the USA and specializing in mortgage, fintech domain, banks of the USA.',
           description: 
           'Worked on 2 projects. '+  
-          'First one is integrational service between Loan Origination Systems. ' +
-          'Second one is web application for automation routine of Bank`s underwriter. ' +
-          'Automated Underwriter Service (AUS) ' +
-          'mortgage, fintech domain, banks of USA (no more details so far because NDA)',
+          'The 1st project was the integrational service between Loan Origination Systems. ' +
+          'The 2nd project was the web application for automation routine of Bank`s underwriter. ' +
+          'Used .NET C#, .NET Core, MySql, Java, Spring, React, Docker, Kafka, Git as main technologies. ' +
+          'Worked in disturbed team where one part of the team was in India and others were in Poland and Belarus' +
+          'Implemented features, fixed bugs, investigated issues, wrote UnitTests, reviewed code. ' +
+          'Participated in daily SCRUM video-calls with the colleagues from India and Poland in English. ' + 
+          'Reported about my work and discussed technical topics in English. ',
           companyWebSite: 'www.icemortgagetechnology.com',
           datesBetween: '2019.08 - 2022.04',
-          descriptionTags: ['.NET', 'C#', "MySql", "Java", "Spring", "React", "Kafka", "Microservices", "ELK" ]
+          descriptionTags: ['.NET', 'C#', "Self-hosted", "API", "Window Scheduler", "RDP", "Deployment", "MySql", "XML", "Configuring",
+           "Java", "Spring", "React", "Kafka", "Microservices", "PostgreSQL", "ELK", "Git" ]
         },
         {
-          title: 'Software Engineer',
+          title: 'Senior Software Engineer',
           company: 'Leapwork',
-          companyMeta: 'This is product copmpany headquarted in Denmark with own product - named LEAPWORK.',
-          description: 'Full-stack development of desktop client-server "LEAPWORK" application for automation testing with no code. (no more details so far because NDA)',
+          companyMeta: 'Product copmpany headquarted in Denmark.',
+          description: 'Full-stack development of desktop client-server application for automation testing with no code. ' +
+          'Used .NET C#, WPF, AKKA.NET, Signal-R, MySql, Git as main technologies. ' +
+          'Worked in disturbed team where the part of team was in Denmark. ' + 
+          'Implemented features, fixed bugs, investigated issues, wrote UnitTests, reviewed code. ' +
+          'Participated in daily SCRUM video-calls with the colleagues from Denmark. ' + 
+          'Reported about my work and discussed techical topics in English. ',
           companyWebSite: 'www.leapwork.com',
           datesBetween: '2018.05 - 2019.08',
-          descriptionTags: ['.NET', 'C#', "WPF", "AKKA.NET", "MySql" ]
+          descriptionTags: ['.NET', 'C#', "WPF", "AKKA.NET", "Signal-R", "MySql", "Git" ]
         },
         {
           title: 'Software Engineer',
           company: 'OptixSoft',
-          companyMeta: 'This is product copmpany headquarted in Belarus and spicializing in maintain of fiber lines.',
-          description: 'Full-stack development of fiber line maintenance application, desktop & web versions. (no more details so far because NDA)',
+          companyMeta: 'Product company headquarted in Belarus and specializing in maintaining of fiber lines.',
+          description: 'Full-stack development of fiber line maintenance application, desktop & web versions.',
           companyWebSite: 'www.optixsoft.com',
           datesBetween: '2016.05 - 2018.05',
-          descriptionTags: ['.NET', 'C#', "Silverlight", "WPF", "Angular 4", "Typescript", "MySql" ]
+          descriptionTags: ['.NET', 'C#', "Silverlight", "WPF", "Angular 4", "Typescript", "MySql", "Git", "SVN" ]
         },
         {
           title: 'Software Engineer',
           company: 'Synesis',
-          companyMeta: 'This is product copmpany headquarted in Russia.',
+          companyMeta: 'Product copmpany headquarted in Russia.',
           description: 'C# .NET game developing in Xamarin for Android and IOS platform. ' +
-          'I worked there on one little project. It is game in race for mobile platform. ' +
-          'Used technology: .NET, Xamarin and company-made game engine. ' +
-          'My responsibilities: fix bugs, add new features. ' +
-          'Team of 4 programmers and 2 testers. ',
+          'Worked on one project. It was the game for a mobile platform. ' +
+          'Used technologies: .NET, Xamarin and company-made game engine. ' +
+          'My responsibilities: fixed bugs, added new features. ' +
+          'Team consisted of 4 programmers and 2 testers. ',
           companyWebSite: 'synesis-group.com',
           datesBetween: '2015.10 - 2016.01',
-          descriptionTags: ['.NET', 'C#', "Xamarin", "Android", "iOS"]
+          descriptionTags: ['.NET', 'C#', "Xamarin", "Android", "iOS", "Git"]
         },
         {
-          title: 'Software Engineer',
+          title: 'Junior Software Engineer',
           company: 'ADVIN',
-          companyMeta: 'This is product copmpany headquarted in Belarus and specializing on x-ray security.',
+          companyMeta: 'Product company headquarted in Belarus and specializing in x-ray security.',
           description: 
           'C# .NET developing security and robotic software. ' +
-          'I there participated in develop on a few projects: ' +
-          'Bagage scanner - robotic x-ray security control bagage with output image in to display. ' +
-          'Control of passangers - robotic x-ray security control human in full growth with output image to display. ' +
-          'Delivery of liquids in electron paramagnetic resonance spectrometer - servomotor installation for transfer liquids in to spectrometer. ' +
+          'Participated in the development on a few projects: ' +
+          'Baggage scanner - robotic x-ray security control of the baggage with its image display on the screen. ' +
+          "Passenger's Control - robotic x-ray security control of people in full growth with their image display on the screen. " +
+          'EPR - XYZ robotic hand with peristaltic pump, spectroanalyser.' +
           'Terascan - medical x-ray installation for irradiation of cancerous tumors. ' +
           
           'Used technology: .Net Framework 4.5, Windows Forms. ' +
-          'My resposibilities: develop new software in order to migrate with old Delphi code in to progressive C# code. ' +
-          'Team of 6 programmers and 2 testers. ',
+          'My resposibilities: developed new software, migrated Delphi code-base to .NET C# code-base. ' +
+          'Team consisted of 6 programmers and 2 testers. ',
           companyWebSite: 'www.advin.by',
           datesBetween: '2014.02 - 2015.10',
-          descriptionTags: ['.NET', 'C#', "WinForms", "C++", "Delphi", "SerialPort", "X-Ray"]
+          descriptionTags: ['.NET', 'C#', "WinForms", "WPF", "C++", "Delphi", "X-Ray", "SerialPort", "Enternet", "Robotics", "Security", "SVN", "Git"]
         },
         {
-          title: 'Software Engineer',
+          title: 'Trainee Software Engineer',
           company: 'Energomera',
-          companyMeta: 'This is product copmpany headquarted in Belarus  and specializing in ASCAPC. Automatic System for Commercial Accounting of Power Consumption.',
+          companyMeta: 'Product copmpany headquarted in Belarus  and specializing in ASCAPC. Automatic System for Commercial Accounting of Power Consumption.',
           description: 'C# .NET developing metrology sofware, cooperation with database. ' +
-          'I worked there on a few projects for inner manufacture needs: Accounting operations manufacture operators in database. ' +
-          'Metrology calibration, verification counters electroenergy. ' +
+          'I worked there on a few projects for inner manufacture needs: '+
+          'Product Lifecycle Management system. ' +
+          'Metrology calibration/verification counters of power consumption. ' +
           'Generation reports. Used technology: .Net Framework 4.5, Windows Forms, ADO.NET ' +
-          'My responsibilities: develop new software. Team of 1 programmer (me). ',
+          'My responsibilities: developed new software. Team consisted of 1 programmer. ',
           companyWebSite: 'energomera.by',
           datesBetween: '2012.08 - 2014.02',
-          descriptionTags: ['.NET', 'C#', "MS SQL", "WinForms", "SerialPort"]
+          descriptionTags: ['.NET', 'C#', "WinForms", "ADO.NET", "SerialPort"]
         }
       ]
     },
@@ -132,7 +143,7 @@ export const CVData = {
       items: [
         {
           authority: 'English',
-          authorityMeta: 'B2 level, a lot of experience of voice communication with customer'
+          authorityMeta: 'B2 level, years of experience of voice communication'
         },
         {
           authority: 'Russian',
@@ -144,7 +155,7 @@ export const CVData = {
       type: 'tag-list',
       title: 'Skills',
       icon: 'rocket',
-      items: ['.NET', 'C#', 'WPF', 'WCF', 'JB dotTrace', 
+      items: ['.NET', 'C#', 'WPF', 'WCF', 'JB dotTrace', 'TeamCity', 'Azure DevOps', 'Jenkins',
       'JB dotPeak', 'MS VS', 'MS VS Code', 'powershell', 'IoC', 
       'Design Patterns', 'SOLID', 'TDD',
       'WinForms', 'Xamarin', 'Silverlight', 'ADO.NET',
@@ -159,7 +170,7 @@ export const CVData = {
       type: 'tag-list',
       title: 'Hobbies & Interests',
       icon: 'cubes',
-      items: ['Tech books about software engineering', 'Radioelectronics/Physics/Math', 'DIY', 'Sport/Cycling/Gym']
+      items: ['Tech books about software engineering', 'Listening music', 'Self-motivation', 'Automatization routine', 'Radioelectronics/Physics/Math', 'DIY', 'Sport/Cycling/Gym']
     }
   ]
 }
